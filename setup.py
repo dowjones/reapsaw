3c0a2ceef5b7456565975fe9ed452ba0081f2730
florykiank <karen.florykian@dowjones.com>
2019-08-06 19:25:16 +0300

    code cleanup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DowJones SAST Container Core',
+    name='DowJones Reapsaw',
     version='1.0.0',
     description='Core component',
     long_description='',
